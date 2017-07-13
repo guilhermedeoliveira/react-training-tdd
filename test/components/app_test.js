c701a430396b6ea57779283cdd520e9c42605bd2
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2017-07-13 20:48:32 -0300

    Create CommentBox component

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -3,14 +3,15 @@ import App from '../../src/components/app';
 
 //Use 'describe' to group together similar tests
 describe('App', () => {
+  let component;
 
+  beforeEach(() => {
+    component = renderComponent(App);
+  })
   // Use 'it' to test a single attribute of a target
-  it('shows the correct test', () => {
-
-    // Create an instance of App
-    const component = renderComponent(App);
+  it('shows a comment box', () => {
     // Use 'expect' to make an "assertion" about a target
-    expect(component).to.contain('React simple starter');
+    expect(component.find('.comment-box')).to.exist;
 
   })
 
