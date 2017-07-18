dbb86cdee790ecf98c1d1ba1b9268d2657b6d99f
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2017-07-18 17:48:15 -0300

    Rewrite teste_helper.js from scratch, beginning with browser like CLI setup

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,36 +1,14 @@
-import _$ from 'jquery';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import TestUtils from 'react-addons-test-utils';
+// Setup testing environment to run like a browser in CLI
 import jsdom from 'jsdom';
-import chai, { expect } from 'chai';
-import chaiJquery from 'chai-jquery';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducers from '../src/reducers';
+import jquery from 'jquery';
 
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
-global.navigator = global.window.navigator;
-const $ = _$(window);
+const $ = jquery(global.window);
 
-chaiJquery(chai, chai.util, $);
+// build render component helper the should render a given react class
 
-function renderComponent(ComponentClass, props = {}, state = {}) {
-  const componentInstance =  TestUtils.renderIntoDocument(
-    <Provider store={createStore(reducers, state)}>
-      <ComponentClass {...props} />
-    </Provider>
-  );
+// build helper for a simulate event
 
-  return $(ReactDOM.findDOMNode(componentInstance));
-}
 
-$.fn.simulate = function(eventName, value) {
-  if (value) {
-    this.val(value);
-  }
-  TestUtils.Simulate[eventName](this[0]);
-};
-
-export {renderComponent, expect};
+// setup chai-jquery
