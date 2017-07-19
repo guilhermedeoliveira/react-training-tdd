eb8d89d65c56521490796d8d78bf5431e3872551
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2017-07-18 21:40:33 -0300

    Complete test_helper.js

--- test/test_helper.js
+++ test/test_helper.js
@@ -4,10 +4,11 @@ import jquery from 'jquery';
 import TestUtils from 'react-addons-test-utils'
 import React from 'react';
 import reactDOM from 'react-dom';
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from '../src/reducers';
+import chaiJquery from 'chai-jquery';
 
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
@@ -25,7 +26,14 @@ function renderComponent(ComponentClass, props, state) {
 }
 
 // build helper for a simulate event
-
+$.fn.simulate = function(eventName, value) {
+  if (value) {
+    this.val(value);
+  }
+  TestUtils.Simulate[eventName](this[0]);
+}
 
 // setup chai-jquery
+chaiJquery(chai, chai.util, $);
+
 export { renderComponent, expect };
