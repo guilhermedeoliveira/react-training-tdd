6ab1c5e7ea4c3c0adda6e585cdf0c0359fd5c379
guilhermedeoliveira <contatoguilhermedeoliveira@gmail.com>
2017-07-18 21:22:03 -0300

    Complete test_helper.js helper renderComponent

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,14 +1,31 @@
 // Setup testing environment to run like a browser in CLI
 import jsdom from 'jsdom';
 import jquery from 'jquery';
+import TestUtils from 'react-addons-test-utils'
+import React from 'react';
+import reactDOM from 'react-dom';
+import { expect } from 'chai';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from '../src/reducers';
 
 global.document = jsdom.jsdom('<!doctype html><html><body></body></html>');
 global.window = global.document.defaultView;
 const $ = jquery(global.window);
 
 // build render component helper the should render a given react class
+function renderComponent(ComponentClass, props, state) {
+  const componentInstance = TestUtils.renderIntoDocument(
+    <Provider store={createStore(reducers, state)}>
+      <ComponentClass {...props}/>
+    </Provider>
+);
+
+  return $(reactDOM.findDOMNode(componentInstance)); // produces HTML
+}
 
 // build helper for a simulate event
 
 
 // setup chai-jquery
+export { renderComponent, expect };
